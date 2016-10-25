59e58aa4ead2fd493a7796c4fda20c226005c51f
Christian Trosell <ct222hv@student.lnu.se>
2016-10-25 14:32:28 +0200

    Implement Stand

--- BlackJack/model/Dealer.cs
+++ BlackJack/model/Dealer.cs
@@ -67,5 +67,24 @@ namespace BlackJack.model
             }
             return false;
         }
+
+        public bool Stand()
+        {
+            if (m_deck != null)
+            {
+                ShowHand();
+
+                while(m_hitRule.DoHit(this))
+                {
+                    Card c = m_deck.GetCard();
+                    c.Show(true);
+                    DealCard(c);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
     }
 }
