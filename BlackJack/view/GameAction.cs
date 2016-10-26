95023631f469efd58d0a0c031454f0ee24f418e7
Christian Trosell <ct222hv@student.lnu.se>
2016-10-26 11:48:20 +0200

    Create enum GameAction

---
+++ BlackJack/view/GameAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJack.view
+{
+    enum GameAction
+    {
+        Play,
+        Hit,
+        Stand,
+        Quit,
+        Invalid
+    }
+}
