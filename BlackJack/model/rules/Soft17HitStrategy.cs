3569a590ebe736c437293b734c506599f7609680
Peter Andersson <pa222ku@student.lnu.se>
2016-10-26 14:32:40 +0200

    Implement Soft17 strategy

---
+++ BlackJack/model/rules/Soft17HitStrategy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJack.model.rules
+{
+    class Soft17HitStrategy : IHitStrategy
+    {
+        private const int HitLimit = 17;
+        private Player _dealer;
+        public bool DoHit(model.Player aDealer)
+        {
+            _dealer = aDealer;
+            var score = _dealer.CalcScore();
+            return score < HitLimit || IsSoft17(score);
+        }
+
+        private bool IsSoft17(int score)
+        {
+            return score == 17 && HasAceCountedAsOne(score);
+        }
+
+        private bool HasAceCountedAsOne(int score)
+        {
+            return _dealer.CalcScore() != CalcLowScore();
+        }
+
+        private int CalcLowScore()
+        {
+            int[] cardScores = new int[(int)model.Card.Value.Count]
+{2, 3, 4, 5, 6, 7, 8, 9, 10, 10 ,10 ,10, 11};
+            int score = 0;
+
+            foreach (Card c in _dealer.GetHand())
+            {
+                if (c.GetValue() != Card.Value.Hidden)
+                {
+                    score += cardScores[(int)c.GetValue()];
+                }
+            }
+            return score;
+        }
+    }
+}
