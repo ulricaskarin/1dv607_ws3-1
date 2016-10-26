1437327dc60de9721ed3fc5445673a9f3d745b31
Christian Trosell <ct222hv@student.lnu.se>
2016-10-26 11:49:35 +0200

    Remove GetInput and replace with GetAction

--- BlackJack/view/IView.cs
+++ BlackJack/view/IView.cs
@@ -8,10 +8,10 @@ namespace BlackJack.view
     interface IView
     {
         void DisplayWelcomeMessage();
-        int GetInput();
         void DisplayCard(model.Card a_card);
         void DisplayPlayerHand(IEnumerable<model.Card> a_hand, int a_score);
         void DisplayDealerHand(IEnumerable<model.Card> a_hand, int a_score);
         void DisplayGameOver(bool a_dealerIsWinner);
+        GameAction GetAction();
     }
 }
