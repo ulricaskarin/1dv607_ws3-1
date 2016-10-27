591da19bf77eb22d6ece8eff173557eb4180689a
ulricaskarin <ls222xp@student.lnu.se>
2016-10-27 09:49:37 +0200

    Implementing method in InternationalNewGameStrategy

--- BlackJack/model/rules/InternationalNewGameStrategy.cs
+++ BlackJack/model/rules/InternationalNewGameStrategy.cs
@@ -10,19 +10,9 @@ namespace BlackJack.model.rules
 
         public bool NewGame(Deck a_deck, Dealer a_dealer, Player a_player)
         {
-            Card c;
-
-            c = a_deck.GetCard();
-            c.Show(true);
-            a_player.DealCard(c);
-
-            c = a_deck.GetCard();
-            c.Show(true);
-            a_dealer.DealCard(c);
-
-            c = a_deck.GetCard();
-            c.Show(true);
-            a_player.DealCard(c);
+            a_dealer.GetShowDealCard(true, a_player);
+            a_dealer.GetShowDealCard(true, a_dealer);
+            a_dealer.GetShowDealCard(true, a_player);
 
             return true;
         }
